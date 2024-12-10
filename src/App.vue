a7a26beced84269f84ee9e7730686e8042543af3
LPtrichor <1985835442@qq.com>
2024-12-10 13:34:29 +0800

    初始化项目结构及基础配置

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+// 测试
+import { healthUsingGet } from '@/api/mainController'
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+
+// 尝试获取用户信息
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+
+
+healthUsingGet().then((res) => {
+  console.log(res)
+})
+
+</script>
