5780955abe250bdb560a5fdfb412ad9fd66afc22
LPtrichor <1985835442@qq.com>
2024-12-11 17:31:08 +0800

    添加用户登录、注册及权限管理功能

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,19 @@
 
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-// 测试
-import { healthUsingGet } from '@/api/mainController'
-import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
-
-// 尝试获取用户信息
-const loginUserStore = useLoginUserStore()
-loginUserStore.fetchLoginUser()
-
-
-
-healthUsingGet().then((res) => {
-  console.log(res)
-})
+// 已经在access.ts文件里面获取了
+// // 测试
+// import { healthUsingGet } from '@/api/mainController'
+// import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+//
+// // 尝试获取用户信息
+// const loginUserStore = useLoginUserStore()
+// loginUserStore.fetchLoginUser()
+//
+//
+//
+// healthUsingGet().then((res) => {
+//   console.log(res)
+// })
 
 </script>
