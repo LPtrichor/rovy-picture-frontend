664457de3b97f97bd67d0109ef7942f81c284573
LPtrichor <1985835442@qq.com>
2024-12-13 22:08:21 +0800

    添加图片管理、搜索及详情展示功能

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -103,6 +103,16 @@ const originItems = [
     label: h('a', { href: 'https://www.nvi.rovy.tech', target: '_blank' }, '编程导航'),
     title: '编程导航',
   },
+  {
+    key: '/add_picture',
+    label: '创建图片',
+    title: '创建图片',
+  },
+  {
+    key: '/admin/pictureManage',
+    label: '图片管理',
+    title: '图片管理',
+  }
 ]
 
 // 过滤菜单项
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,19 @@ import UserManagePage from '@/pages/admin/UserManagePage.vue'
 import UserRegisterPage from '@/pages/user/UserRegisterPage.vue'
 import UserLoginPage from '@/pages/user/UserLoginPage.vue'
 import HomePage from '@/pages/HomePage.vue'
+import AddPicturePage from '@/pages/AddPicturePage.vue'
+import PictureManagePage from '@/pages/admin/PictureManagePage.vue'
+import PictureDetailPage from '@/pages/PictureDetailPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/picture/:id',
+      name: '图片详情',
+      component: PictureDetailPage,
+      props: true,
+    },
     {
       path: '/',
       name: '主页',
@@ -28,6 +37,16 @@ const router = createRouter({
       name: '用户管理',
       component: UserManagePage,
     },
+    {
+      path: '/add_picture',
+      name: '创建图片',
+      component: AddPicturePage,
+    },
+    {
+      path: '/admin/pictureManage',
+      name: '图片管理',
+      component: PictureManagePage,
+    }
   ],
 })
 
